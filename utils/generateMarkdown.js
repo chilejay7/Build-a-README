37de95a87190fc527069e06c2092edecde408747
chilejay7 <codyburk7@yahoo.com>
2023-10-22 11:34:00 -0600

    comments added to script file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -31,6 +31,7 @@ renderLicenseSection = (license) => {
 // This function is used to generate the Table of Contents from the array created through the inquirer prompt.
 generateTable = (tableOfContents) => {
   if (tableOfContents !== null || tableOfContents !== undefined) {
+    // The empty variable is needed to hold and return all of the values created through the for loop.  The values are created following the link formatting needed for the markdown language.  The actual link itself has to be lowercase, which is why the toLowerCase method is required.
     let table = ''
     for (let i = 0; i < tableOfContents.length; i++) {
       console.log(tableOfContents[i]);
